59ad151e9190cca00739c700991fac8618053309
mcascone <max@bitovi.com>
2023-05-15 13:54:39 -0500

    azure plugin first steps

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "Running Azure Plugin deployment script..."
+
+# vars
+export BITOPS_SCHEMA_ENV_FILE="$BITOPS_OPSREPO_ENVIRONMENT_DIR/ENV_FILE"
+
+if [ ! -d "$BITOPS_OPSREPO_ENVIRONMENT_DIR" ]; then
+  echo "No azure plugin directory. Skipping."
+  exit 0
+fi
+
+echo "Deploying azure plugin..."
+
+if [ ! -f "$BITOPS_SCHEMA_ENV_FILE" ]; then 
+  echo "No azure plugin ENV file found"
+else
+  source "$BITOPS_SCHEMA_ENV_FILE"
+fi
+
+cd $BITOPS_OPSREPO_ENVIRONMENT_DIR
+
+echo "Listing contents of azure plugin Root: $BITOPS_OPSREPO_ENVIRONMENT_DIR"
+ls -al .
+
+
+echo "Running the plugin CLI: (SKIPPED)"
+
+echo "Options:"
+echo "AZURE_FOO: $AZURE_FOO"
+echo "AZURE_BAR: $AZURE_BAR"
